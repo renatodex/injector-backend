2cf2699a1dddacfaee5359e30dcb8be3701316d7
Renato <renatodex@MacBook-Pro-de-Renato.local>
2015-09-15 02:27:19 -0300

    Added virtual attribute to sumary/page scope

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,5 +2,9 @@ class Page < ActiveRecord::Base
   belongs_to :account
   has_many :page_injections
 
-  scope :sumary, -> { limit(5).order(created_at: :desc) } 
+  scope :sumary, -> { limit(5).order(created_at: :desc).as_json(methods: :total_injections) }
+
+  def total_injections
+    self.page_injections.count
+  end
 end
