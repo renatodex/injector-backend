aa567151cd8146568b18c5aa3b4499e60dba3957
Renato <renatodex@MacBook-Pro-de-Renato.local>
2015-09-17 00:50:41 -0300

    Added injection type name to sumary injection api

--- app/models/injection.rb
+++ app/models/injection.rb
@@ -3,5 +3,9 @@ class Injection < ActiveRecord::Base
   belongs_to :injection_type
   has_many :page_injections
 
-  scope :sumary, -> { limit(5).order(created_at: :desc) }
+  scope :sumary, -> { limit(5).order(created_at: :desc).as_json(methods: :injection_type_name) }
+
+  def injection_type_name
+    self.injection_type.name
+  end
 end
